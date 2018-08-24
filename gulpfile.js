f9741eaff470067f215ebe5e16fd7559fef6f23c
voyula <40173603+voyula@users.noreply.github.com>
2018-08-24 06:53:28 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,24 @@
-﻿'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const minifyCSS = require('gulp-csso');
 
-gulp.task('library-materialize-css', () => {
-    return gulp.src('node_modules/materialize-css/dist/**/*')
-        .pipe(gulp.dest('dist/libs/materialize-css'));
-});
+gulp.task('library-materialize-css', () => gulp.src('node_modules/materialize-css/dist/**/*')
+  .pipe(gulp.dest('dist/libs/materialize-css')));
 
-gulp.task('library-jquery', () => {
-    return gulp.src('node_modules/jquery/dist/jquery.min.js')
-        .pipe(gulp.dest('dist/libs'));
-});
+gulp.task('library-jquery', () => gulp.src('node_modules/jquery/dist/jquery.min.js')
+  .pipe(gulp.dest('dist/libs')));
 
-gulp.task('css', () => {
-    return gulp.src('src/web/client.scss')
-        .pipe(sass())
-        .pipe(concat('client.min.css'))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('css', () => gulp.src('src/web/client.scss')
+  .pipe(sass())
+  .pipe(concat('client.min.css'))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('dist')));
 
-gulp.task('js', () => {
-    return gulp.src('src/web/client.js')
-        .pipe(uglify())
-        .pipe(concat('client.min.js'))
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('js', () => gulp.src('src/web/client.js')
+  .pipe(uglify())
+  .pipe(concat('client.min.js'))
+  .pipe(gulp.dest('dist')));
 
-gulp.task('default', [ 'library-materialize-css', 'library-jquery', 'css', 'js' ]);
+gulp.task('default', ['library-materialize-css', 'library-jquery', 'css', 'js']);
